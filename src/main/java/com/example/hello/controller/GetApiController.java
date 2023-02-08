65acdeeaa6317f518205dd435360c779cf29a4af
Jungsik Won <wl808@naver.com>
2023-02-08 17:33:09 +0900

    UserRequest Query-Param

--- src/main/java/com/example/hello/controller/GetApiController.java
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,9 +1,9 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.UserRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -17,5 +17,52 @@ public class GetApiController {
     @RequestMapping(path = "/hi", method = RequestMethod.GET) // http://localhost:8080/api/get/hi
     public String hi(){
         return "get hi";
+    }
+
+    @GetMapping("/path-variable/{id}")
+    public String pathVariable(@PathVariable(name = "id") String pathName){
+        System.out.println("PathVariable : " + pathName);
+
+        return pathName;
+    }
+
+    // http://localhost:8080/api/get/query-param?name=steve&email=wl808@naver.com&age=24
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
+
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach(entry ->{
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println();
+
+            sb.append(entry.getKey()).append(" = ").append(entry.getValue()).append("\n");
+        });
+
+        return sb.toString();
+    }
+
+    @GetMapping(path = "query-param02")
+    public String queryParam02(
+            @RequestParam   String name,
+            @RequestParam   String email,
+            @RequestParam   int age
+    ){
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name + " " + email + " " + age;
+    }
+
+    // ?name = steve & email = wl808@naver.com & age = 24
+    @GetMapping(path = "query-param03")
+    public String queryParam03(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
     }
 }
