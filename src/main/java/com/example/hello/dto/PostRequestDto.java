794703d87819894610b672877e19310df3e15fed
Jungsik Won <wl808@naver.com>
2023-02-25 19:21:26 +0900

    PutMapping

--- src/main/java/com/example/hello/dto/PostRequestDto.java
+++ src/main/java/com/example/hello/dto/PostRequestDto.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PostRequestDto {
     private String account;
+
+    @JsonProperty("e-mail")
     private String email;
+
     private String address;
     private String password;
 
