8e42dd9242f6ecd4f307dadc61d54e501a007f23
Jungsik Won <wl808@naver.com>
2023-03-05 18:55:38 +0900

    0305

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -3,9 +3,12 @@ package com.example.hello;
 import com.example.hello.di.ApplicationContextProvider;
 import com.example.hello.di.Base64Encoder;
 import com.example.hello.di.Encoder;
+import com.example.hello.di.UrlEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class HelloApplication {
@@ -14,12 +17,30 @@ public class HelloApplication {
         SpringApplication.run(HelloApplication.class, args);
 
         ApplicationContext context = ApplicationContextProvider.getContext();
-        Base64Encoder base64Encoder = context.getBean(Base64Encoder.class);
-        Encoder encoder = new Encoder(base64Encoder);
+        // Base64Encoder base64Encoder = context.getBean(Base64Encoder.class);
+        // Encoder encoder = new Encoder(base64Encoder);
+
+        // IoC (제어의 역전)
+        Encoder encoder = context.getBean(Encoder.class);
 
         String url = "www.naver.com/books/it?page=10&size=20";
         String result = encoder.encode(url);
         System.out.println(result);
     }
 
+}
+
+// Bean -> Spring Container (IoC)
+@Configuration
+class AppConfig {
+
+    @Bean("base64Encode")
+    public Encoder encoder(Base64Encoder base64Encoder){
+        return new Encoder(base64Encoder);
+    }
+
+    @Bean("urlEncode")
+    public Encoder encoder(UrlEncoder urlEncoder){
+        return new Encoder(urlEncoder);
+    }
 }
--- src/main/java/com/example/hello/di/Encoder.java
+++ src/main/java/com/example/hello/di/Encoder.java
@@ -1,9 +1,13 @@
 package com.example.hello.di;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Encoder {
     private IEncoder iEncoder;
 
-    public Encoder(IEncoder iEncoder){
+    public Encoder(@Qualifier("urlEncoder") IEncoder iEncoder){
         this.iEncoder = iEncoder;
     }
     public String encode(String message){
